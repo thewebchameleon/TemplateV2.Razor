d69547b595bddd2ad4124319b3833612ca32c179
Adrian Brink <abrink@finchoice.co.za>
2019-11-18 19:54:42 +0200

    fixed reset password

--- TemplateV2.Razor/Pages/Account/ResetPassword.cshtml.cs
+++ TemplateV2.Razor/Pages/Account/ResetPassword.cshtml.cs
@@ -1,17 +1,60 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using TemplateV2.Models.ServiceModels.Account;
+using TemplateV2.Services.Contracts;
 
 namespace TemplateV2.Razor.Pages
 {
     public class ResetPasswordModel : BasePageModel
     {
+        #region Private Fields
+
+        private readonly IAccountService _service;
+
+        #endregion
+
+        #region Properties
+
+        [BindProperty]
+        public ResetPasswordRequest FormData { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Token { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public ResetPasswordModel(IAccountService service)
+        {
+            _service = service;
+        }
+
+        #endregion
+
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            FormData.Token = Token;
+
+            if (ModelState.IsValid)
+            {
+                var response = await _service.ResetPassword(FormData);
+                if (response.IsSuccessful)
+                {
+                    AddNotifications(response);
+                    return RedirectToLogin();
+                }
+                else
+                {
+                    AddFormErrors(response);
+                }
+            }
+            return Page();
         }
     }
 }
