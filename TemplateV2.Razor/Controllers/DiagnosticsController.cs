7b67688ba73252088c1c7d3d29d911e77eeffc9b
Adrian Brink <adrian.brink@outlook.com>
2019-12-03 18:16:23 +0200

    added ping method

---
+++ TemplateV2.Razor/Controllers/DiagnosticsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TemplateV2.Razor.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class DiagnosticsController : BaseController
+    {
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok("Pong");
+        }
+    }
+}
